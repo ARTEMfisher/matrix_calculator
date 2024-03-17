949c9b1a0d5869c89cb7234ac304fde9cc6edfe5
unknown <artem.b_2005@mail.ru>
2024-03-18 00:37:35 +0300

    -

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 A=[]
 B=[]
 C=[]
@@ -14,21 +14,4 @@ heightA = 0
 widthB = 0
 heightB = 0
 
-=======
-A=[]
-B=[]
-C=[]
 
-vectorA=[]
-vectorB=[]
-vectorC=[]
-
-
-widthA = 0
-heightA = 0
-
-widthB = 0
-heightB = 0
-
-coordinate =['x', 'y', 'z']
->>>>>>> e971cce98d26ac2d3e184dffb5cbf7d7d3c900ce
